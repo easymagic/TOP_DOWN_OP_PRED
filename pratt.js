bae56be23121a5d538cc80af9e301e1d706354fb
easymagic <nnamware@yahoo.com>
2017-01-04 13:16:59 -0800

    Update pratt.js

--- pratt.js
+++ pratt.js
@@ -1,11 +1,13 @@
 var pratt = {};
+var function_scope = {}; //global function scope declaration made here ...
+var symbols = {};
 
 (function(p){
  
  var binding_powers = {};
  var tokens = ['4','+','11','/','(','13','-','2',')'];
  var pointer = -1;
- var splitters = ['-','+','/','*','(',')','[',']','if','then','else','pow','ceil','floor','sqr','sqrt','function','{','}',';','=','.eq.'];
+ var splitters = ['print','>=','<=','==','++','--','!=','+=','-=','*=','/=','<>','>','"','<','-','+','/','*','(',')','[',']',',','if','then','else','for','pow','ceil','floor','sqr','sqrt','function','{','}',';','=','.eq.'];
  var symbol_proto = {
  	nud:function(){
 
@@ -18,10 +20,13 @@ var pratt = {};
 
  var token = null;
 
- var symbols = {};
+ // var symbols = {};
+ var symbols_obj = [];
 
  p.tokenize = function(input){
    var r = input;
+    r = r.trim();
+    r = r.split("\n").join("").split("\r").join("");
    var glue = '__glue__';
    for (var i in splitters){
       r = r.split(splitters[i]).join(glue + splitters[i] + glue);
@@ -31,7 +36,7 @@ var pratt = {};
    tokens = [];
    for (var i in r){
       if (r[i].trim() != ''){
-         tokens.push(r[i]);
+         tokens.push(r[i].trim());
       }
    }
 
@@ -39,24 +44,59 @@ var pratt = {};
 
  };
 
+
+
+ p.tokenize_adv = function(input){
+   var r = input;
+    r = r.trim();
+    r = r.split("\n").join("").split("\r").join("");
+   var glue = '__glue__';
+   for (var i in splitters){
+      r = r.split(splitters[i]).join(glue + i + 'trk' + glue);
+   }
+
+   for (var i in splitters){
+      r = r.split(glue + i + 'trk' + glue).join(glue + splitters[i] + glue);
+   }
+
+   r = r.split(glue);
+   
+   tokens = [];
+   for (var i in r){
+      if (r[i].trim() != ''){
+         tokens.push(r[i]);
+      }
+   }
+
+    //console.log(tokens);
+
+ };
+
+
  
  p.symbol = function(cfg){
 
  	var id = cfg.id;
+  //console.log(id);
 
  	if (symbols[id]){
       symbols[id].bp = cfg.bp;
+      if (cfg.nud){
+        symbols[id].nud = cfg.nud;
+      }
  	}else{
 
  	  symbols[id] = Object.create(symbol_proto);
  	  symbols[id].bp = cfg.bp;
  	  symbols[id].value = cfg.id;
  	  symbols[id].nud = cfg.nud || function(){
- 	  	return id;
+ 	  	return id.trim();
  	  };
  	  symbols[id].led = cfg.led || symbols[id].led;
 
  	}
+
+  symbols_obj[pointer+1] = symbols[id];
    
  }; 
 
@@ -67,15 +107,24 @@ var pratt = {};
      //pointer = tokens.length - 1;
      return;
    }
+
+   //console.log(tokens[pointer]);
    
-   if (!symbols[tokens[pointer]]){
+   if (!symbols[tokens[pointer].trimLeft()]){
+     //console.log(tokens[pointer]);
      this.symbol({
      	bp:0,
-     	id:tokens[pointer]
+     	id:tokens[pointer].trimLeft()
      });
+   }else{
+    //console.log(tokens[pointer]);
    }
 
-   token = symbols[tokens[pointer]];
+   token = symbols[tokens[pointer].trimLeft()];
+   if (token.value == 'sum'){
+     //console.log(token.nud(),token);
+   }
+   
 
    // console.log(pointer,tokens[pointer]);
 
@@ -93,6 +142,8 @@ var pratt = {};
   }
   token = symbols[tokens[pointer]];
  };
+ 
+ p.prev_token = {};
 
  p.expr = function(rbp){
   var t = token;
@@ -104,7 +155,56 @@ var pratt = {};
      left = t.led(left);
   }
 
+  p.prev_token = left;
+
   return left;
+
+ };
+
+
+ //init predefined functions here...
+ p.init_user_defined_functions = function(id){
+  var fs = function_scope;
+  var id = id.trim();
+  //for (var i in fs){
+   
+   p.symbol({
+    id:id,
+    bp:0,
+    nud:function(){
+      //return 'nud';
+      var rr = {};
+      rr.node = '_function_call_';
+      rr.args = fs[id].args;
+      rr.body = fs[id].body;
+      p.next();
+      var args_ = [];
+      do{ 
+         var tt = p.expr(0);
+         if (tt != ','){
+           args_.push(tt);
+         }
+      }while(token.value != ')');
+      p.next();
+      if (token.value == ';'){
+        p.next();
+      }
+      rr.args_activation = args_;
+
+      //console.log(rr);
+
+      return rr; 
+
+    }
+
+   });
+
+   //console.log(id);
+
+
+  //}
+
+  //console.log(symbols);
 
  };
 
@@ -133,6 +233,67 @@ var pratt = {};
       //return left * p.expr(20);
  	}
  });
+
+
+ p.symbol({
+  id:'++',
+  bp:0,
+  nud:function(){
+    //p.next();
+    var rr = {};
+
+    var rr = {};
+    if (token.value == ';'){
+       rr.dir = 'post';
+       //console.log(symbols_obj);
+       console.log(p.prev_token);
+       rr.left = p.prev_token;
+
+    }else{
+       rr.dir = 'pre';
+       rr.right = p.expr(0);
+    }
+
+
+
+
+    rr.node = '++_vl_';
+    //rr.right = p.expr(0);
+    if (token.value == ';'){
+     p.next();
+    }
+    return rr;
+  }
+ });
+
+
+ p.symbol({
+  id:'--',
+  bp:0,
+  nud:function(){
+    //p.next();
+    var rr = {};
+    if (token.value == ';'){
+       rr.dir = 'post';
+       //console.log(symbols_obj);
+       rr.left = p.prev_token.left.trim();
+    }else{
+       rr.dir = 'pre';
+       rr.right = p.expr(0);
+    }
+
+    //console.log(rr);
+
+    rr.node = '--_vl_';
+    //rr.right = p.expr(0);
+    if (token.value == ';'){
+     p.next();
+    }
+    return rr;
+  }
+ });
+
+
 
 
  p.symbol({
@@ -194,6 +355,36 @@ var pratt = {};
 
 
  p.symbol({
+  id:'print',
+  bp:0,
+  nud:function(){
+    var rr = {};
+    p.next();
+    var v = [];
+    do{
+      
+      var r_ = p.expr(0);
+
+      if (r_ != ','){
+       v.push(r_);
+      }
+      
+      var tk = token.value;
+
+    }while(tk != ')');
+    p.next();
+    if (token.value == ';'){
+     p.next();
+    }
+    rr.right = v;  
+    rr.node = 'print';
+    return rr;
+  }
+ });
+
+
+
+ p.symbol({
   id:"{",
   bp:0,
   nud:function(){
@@ -205,13 +396,21 @@ var pratt = {};
       do {
 
          var rr = p.expr(0);
+         
          body.push(rr);
-         //var id = token.value;
-         p.next();
+
+         
    
       }while(token.value != '}');
 
+      //console.log(token.value);
+
         p.next();
+
+    while (token.value == ';'){
+     p.next();
+    }
+
 
       return {
          right:body,
@@ -228,25 +427,208 @@ var pratt = {};
   led:function(left){
     var r = {};
     r.left = left;
+    //console.log(symbols);
     r.right = p.expr(0);
     r.node = '=';
+    //console.log(token.value);
     //console.log(r);
+    if (token.value == ';'){
+       p.next();
+    }
     return r;
   }
  });
 
 
  p.symbol({
-  id:'.eq.',
+  id:'+=',
+  bp:5,
+  led:function(left){
+    var r = {};
+    r.left = left;
+    r.node = '+=';
+    r.right = p.expr(0);
+    if (token.value == ';'){
+       p.next();
+    }
+    return r;
+  }
+ });
+
+ p.symbol({
+  id:'-=',
+  bp:5,
+  led:function(left){
+    var r = {};
+    r.left = left;
+    r.node = '-=';
+    r.right = p.expr(0);
+    if (token.value == ';'){
+       p.next();
+    }
+    return r;
+  }
+ });
+
+
+ p.symbol({
+  id:'*=',
+  bp:5,
+  led:function(left){
+    var r = {};
+    r.left = left;
+    r.node = '*=';
+    r.right = p.expr(0);
+    if (token.value == ';'){
+       p.next();
+    }
+    return r;
+  }
+ });
+
+
+ p.symbol({
+  id:'/=',
+  bp:5,
+  led:function(left){
+    var r = {};
+    r.left = left;
+    r.node = '/=';
+    r.right = p.expr(0);
+    if (token.value == ';'){
+       p.next();
+    }
+    return r;
+  }
+ });
+
+
+
+ p.symbol({
+  id:'==',
   bp:5,
   led:function(left){
     var r = {};
     r.left = left;
     r.right = p.expr(0);
-    r.node = '.eq.';
+    r.node = '==';
     return r;
   }
  });
+
+
+ p.symbol({
+  id:'<=',
+  bp:5,
+  led:function(left){
+    var r = {};
+    r.left = left;
+    r.right = p.expr(0);
+    r.node = '<=';
+    return r;
+  }
+ });
+
+ p.symbol({
+  id:'>=',
+  bp:5,
+  led:function(left){
+    var r = {};
+    r.left = left;
+    r.right = p.expr(0);
+    r.node = '>=';
+    return r;
+  }
+ });
+
+
+ p.symbol({
+  id:'>',
+  bp:5,
+  led:function(left){
+    var r = {};
+    r.left = left;
+    r.right = p.expr(0);
+    r.node = '>';
+    return r;
+  }
+ });
+
+
+  p.symbol({
+  id:'<',
+  bp:5,
+  led:function(left){
+    var r = {};
+    r.left = left;
+    r.right = p.expr(0);
+    r.node = '<';
+    return r;
+  }
+ });
+
+
+
+ p.symbol({
+  id:'!=',
+  bp:5,
+  led:function(left){
+    var r = {};
+    r.left = left;
+    r.right = p.expr(0);
+    r.node = '!=';
+    return r;
+  }
+ });
+
+
+
+ p.symbol({
+  id:'<>',
+  bp:5,
+  led:function(left){
+    var r = {};
+    r.left = left;
+    r.right = p.expr(0);
+    r.node = '<>';
+    return r;
+  }
+ });
+
+
+
+
+ p.symbol({
+  id:'"',
+  bp:0,
+  nud:function(){
+    var r = {};
+    var b = [];
+
+    r.node = '(string)';
+    //return r;
+
+
+    do{
+
+      b.push(token.value);
+
+      p.next();
+
+    }while(token.value != '"');
+
+    p.next();
+
+    r.buff = b;
+
+    return r;
+
+  }
+
+ });
+
+
+
 
 
  p.symbol({
@@ -260,7 +642,7 @@ var pratt = {};
     r.action = p.expr(0);
     r.node = 'if';
     //p.next();
-    //console.log(token.value);
+   // console.log(token.value);
     if (token.value == 'else'){
       p.next();
       r.else_part = p.expr(0);
@@ -271,6 +653,78 @@ var pratt = {};
     return r;
   }
  });
+
+
+ p.symbol({
+  id:'for',
+  bp:0,
+  nud:function(){
+    p.next();
+    var rr = {};
+    rr.start = p.expr(0);
+    rr.stop = p.expr(0); 
+    p.next();
+    rr.inc = p.expr(0);
+    p.next();
+    rr.node = 'for';
+    rr.body = p.expr(0); 
+    return rr;
+  }
+ });
+
+
+ p.symbol({
+  id:'function',
+  bp:0,
+  nud:function(){
+    var rr = {};
+    rr.node = 'function';
+    rr.function_name = token.value.trim();
+
+    var args = [];
+
+    
+    p.next();
+    p.next();
+
+    do{
+      
+      var arg_ = p.expr(0);
+      if (arg_ != ','){
+        args.push(arg_);
+      }
+
+      //p.next();
+      
+
+    }while(token.value != ')');
+
+    p.next();
+
+    rr.args = args;
+
+    rr.body = p.expr(0);
+
+    function_scope[rr.function_name] = {
+      args:args,
+      body:rr.body
+    };
+
+
+    p.init_user_defined_functions(rr.function_name);
+
+    //console.log(rr.function_name);
+
+    return rr;
+  }
+
+ });
+
+
+
+
+
+
 
 
 
@@ -291,10 +745,12 @@ var pratt = {};
 
  
  p.run = function(cmd){
-  this.tokenize(cmd);
+  this.tokenize_adv(cmd);
   this.reset();
   this.next();
-  return this.expr(0);
+  var r = this.expr(0);
+  //this.init_user_defined_functions(); //load user defined functions. 
+  return r; 
  };
 
 
@@ -303,3 +759,261 @@ var pratt = {};
 
 
 })(pratt);
+
+
+
+
+
+
+var interpreter = {};
+(function(itr){
+ 
+  var scope = {};
+  
+
+
+  itr.eval_ = function(ast){
+    //console.log(ast);
+     if (typeof(ast) == 'object'){
+       if (typeof(itr[ast.node]) != 'undefined'){
+         return itr[ast.node].apply(itr,[ast]);
+       }else{
+         return 'Lib not defined!';
+       }
+     }else{
+       if (typeof(scope[ast]) != 'undefined'){
+          return scope[ast];
+       }else{
+          return ast;    
+       }
+     }
+  };
+
+  itr['='] = function(ast){
+    scope[ast.left] = this.eval_.apply(this,[ast.right]);
+    return scope[ast.left];
+  };
+
+  itr['print'] = function(ast){
+
+     var list = ast.right;
+     var rr = [];
+     
+     for (var i in list){
+        rr.push(itr.eval_.apply(this,[list[i]]));
+     }
+
+     //var r = itr.eval_.apply(this,[ast.right]);
+     //console.log(rr);
+     return rr.join(' , ');
+
+  };
+
+
+  itr['*'] = function(ast){
+    return itr.eval_(ast.left) * itr.eval_(ast.right);
+  };
+
+  itr['+'] = function(ast){
+    return (+itr.eval_(ast.left)) + (+itr.eval_(ast.right));
+  };
+
+  itr['/'] = function(ast){
+    return (+itr.eval_(ast.left)) / (+itr.eval_(ast.right));
+  };
+
+  itr['-'] = function(ast){
+    return (+itr.eval_(ast.left)) - (+itr.eval_(ast.right));
+  };
+
+
+  itr['('] = function(ast){
+    return (+itr.eval_(ast.left));
+  };
+
+  itr['{'] = function(ast){
+    var arr = ast.right;
+    var r = '';
+    for (var i in arr){
+      r = itr.eval_(arr[i]);
+    }
+    return r;
+  };
+
+  itr['<='] = function(ast){
+     return (itr.eval_(ast.left)*1 <= itr.eval_(ast.right)*1);
+  };
+
+  itr['>='] = function(ast){
+     return (itr.eval_(ast.left) >= itr.eval_(ast.right));
+  };
+
+
+  itr['<'] = function(ast){
+     return (itr.eval_(ast.left) < itr.eval_(ast.right));
+  };
+
+  itr['>'] = function(ast){
+     return (itr.eval_(ast.left) > itr.eval_(ast.right));
+  };
+
+
+  itr['<>'] = function(ast){
+     return (itr.eval_(ast.left) != itr.eval_(ast.right));
+  };
+
+
+  itr['!='] = function(ast){
+     return (itr.eval_(ast.left) != itr.eval_(ast.right));
+  };
+
+  itr['=='] = function(ast){
+     return (itr.eval_(ast.left)*1 == itr.eval_(ast.right)*1);
+  };
+
+
+  itr['if'] = function(ast){
+    
+    //cond , action , else_part
+    var cond = itr.eval_(ast.cond);
+
+    if (cond){
+      return itr.eval_(ast.action);
+    }else{
+      if (ast.else_part){
+        return itr.eval_(ast.else_part);
+      }
+    }
+
+  };
+
+
+  itr['for'] = function(ast){
+     var start = ast.start;
+     var stop = ast.stop;
+     var inc = ast.inc;
+     var body = ast.body;
+     var r = '';
+
+      for (itr.eval_(start);itr.eval_(stop);itr.eval_(inc)){
+        r =itr.eval_(body);
+      }
+
+      return r;
+  };
+
+  itr['_vl_++'] = function(ast){
+    scope[ast.left]++;
+    return scope[ast.left];
+  };
+
+  itr['++_vl_'] = function(ast){
+    //console.log(ast);
+
+    if (ast.dir == 'post'){
+      
+      var v = scope[ast.left]++; 
+      return v;
+      //console.log(scope[ast.left]);
+      ///return scope[ast.left];
+
+    }else{
+      
+      return ++scope[ast.right];
+      //scope[ast.right];
+      //return scope[ast.right];
+
+    }
+
+
+  };
+
+
+  itr['--_vl_'] = function(ast){
+
+    //console.log(ast);
+    
+    if (ast.dir == 'post'){
+      
+      var v = scope[ast.left]--; 
+      return v;
+      //console.log(scope[ast.left]);
+      //return scope[ast.left];
+
+    }else{
+      
+      return --scope[ast.right];
+      //scope[ast.right];
+      
+
+    }
+    
+    
+  };
+
+  itr['_vl_--'] = function(ast){
+    scope[ast.left]--;
+    return scope[ast.left];
+  };
+
+
+
+  itr['+='] = function(ast){
+     var left = ast.left;
+     var right = ast.right;
+     scope[left] = +scope[left]*1 + itr.eval_(right)*1;
+     return scope[left];
+  };
+
+
+  itr['*='] = function(ast){
+     var left = ast.left;
+     var right = ast.right;
+     scope[left] = +scope[left] * itr.eval_(right);
+     return scope[left];
+  };
+
+
+  itr['-='] = function(ast){
+     var left = ast.left;
+     var right = ast.right;
+     scope[left] = +scope[left]*1 - (+itr.eval_(right))*1;
+     return scope[left];
+  };
+
+
+  itr['/='] = function(ast){
+     var left = ast.left;
+     var right = ast.right;
+     scope[left] = +scope[left] / itr.eval_(right) * 1;
+     return scope[left];
+  };
+
+
+  itr['_function_call_'] = function(ast){
+     
+     var args = ast.args;
+     var args_activation = ast.args_activation;
+     var body = ast.body;
+
+     for (var i in args){
+           scope[args[i]] = itr.eval_(args_activation[i]);
+     }
+
+     return itr.eval_(body);
+
+  };
+
+
+  // itr['function'] = function(ast){
+  //   function_scope[ast.function_name] = {
+  //     args:ast.args,
+  //     body:ast.body
+  //   };  
+  //   console.log(function_scope);
+  // };
+
+
+
+
+})(interpreter);
